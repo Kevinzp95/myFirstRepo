50e5c07890140d002c351e437815c3a2be54ba4c
Alex Kuznetsov <a.kuznetsov23@gmail.com>
2022-02-01 19:06:29 +0200

    Исправил переменные

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+const title = 1
+
+let screens = 2
+    screenPrice = 3
+    rollback = 4
+    fullPrice = 5
+    adaptive = 6
+
 alert('Script is working!');
 console.log('The last, but not the list');
-
-let = title
-const = screens
-let = screenPrice
-let = rollback
-let = fullPrice
-let = adaptive
-
 
 
