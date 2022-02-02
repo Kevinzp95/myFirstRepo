b1093ee5eff40574e984021543cd100457a7baff
Alex Kuznetsov <a.kuznetsov23@gmail.com>
2022-02-02 13:24:03 +0200

    объединил вывод screens с нижним регистром и вывел в виде массива + сделал конкатенацию

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ let adaptive = true;
 
 console.log(typeof title, typeof fullPrice, typeof adaptive);
 console.log(screens.length);
-console.log("Стоимость верстки экранов", screenPrice, "долларов");
-console.log("Стоимость разработки сайта", fullPrice, "долларов");
-console.log(screens.toLowerCase());
-console.log(screens.split("ы"));
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "долларов");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "долларов");
+console.log(screens.toLowerCase().split(","));
 
 let percent ;
 percent = fullPrice * (rollback/100);
