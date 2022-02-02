694fc5313cf92a2c4f0d32ba2e27a8b385ee01f3
Alex Kuznetsov <a.kuznetsov23@gmail.com>
2022-02-02 11:45:23 +0200

    Исправил скрипт согласно  замечаниям

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const title = 'myFirstProject';
 
 let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 3;
+let screenPrice = 1500;
 let rollback = 63;
 let fullPrice = 5000;
 let adaptive = true;
@@ -9,9 +9,10 @@ let adaptive = true;
 
 console.log(typeof title, typeof fullPrice, typeof adaptive);
 console.log(screens.length);
-console.log(screenPrice, fullPrice);
+console.log("Стоимость верстки экранов", screenPrice, "долларов");
+console.log("Стоимость разработки сайта", fullPrice, "долларов");
 console.log(screens.toLowerCase());
-console.log(screens.split());
+console.log(screens.split("ы"));
 
 let percent ;
 percent = fullPrice * (rollback/100);
