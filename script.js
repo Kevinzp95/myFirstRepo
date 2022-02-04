e9a0c3cd9a389feb8b9b4fbffde989ee07316782
Alex Kuznetsov <a.kuznetsov23@gmail.com>
2022-02-04 18:49:00 +0200

    Внес правки в скрипт

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
 'use strict'
 
-let title = 'myFirstProject';
-let aim = 'цель'
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 1500;
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов необходимо разработать?', "Простые, Сложные, Интерактивные?")
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let rollback = 63;
 let fullPrice = 3000;
-let service1 = 300;
-let service1Price = 26;
-let adaptive = true;
-let adaptivePrice = 30;
+let adaptive = confirm("Нужен ли адаптив на сайте?"); 
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let service1Price = +prompt('Сколько будет стоить дополнительная услуга?');
+let service2 = prompt('Какой еще дополнительный тип услуги нужен?');
+let service2Price = +prompt('Сколько будет стоить данная дополнительная услуга?');
 
-
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "долларов");
-console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "долларов");
-console.log(screens.toLowerCase().split(","));
 
   let percent;
       percent = fullPrice * (rollback/100);
       console.log(percent);
 
 
-// -------  Задание 3-его урока ----------
-
-
-title = prompt('Как называется ваш проект?');
-aim = prompt('Какова цель вашего проекта?');
-screens = prompt('Какие типы экранов необходимо разработать?', "Простые, Сложные, Интерактивные?")
-screenPrice = prompt('Сколько будет стоить данная работа?'); 
-service1 = prompt('Какой дополнительный тип услуги нужен?'); 
-service1Price = +prompt('Сколько будет стоить дополнительная услуга?'); 
-adaptive = confirm("Нужен ли адаптив на сайте?"); 
-
-  switch (true) {
-      case adaptive == true:
-            adaptivePrice = prompt('Сколько будет стоить адаптив?');
-            break
-      case adaptive == false:
-            alert('Жаль, адаптивность улучшила бы ваш проект');
-}
-
 fullPrice = screenPrice + service1Price;
-console.log(fullPrice);
 
   let servicePercentPrice = fullPrice - percent;
   console.log(Math.ceil(servicePercentPrice));
